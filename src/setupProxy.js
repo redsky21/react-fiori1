22407c560a465bc410ccdc56cad5e783470006c0
pig1 <redsky21@lgcns.com>
2022-10-03 22:11:00 +0900

    4123

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,10 +3,10 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
       createProxyMiddleware('/api', {
-        target: 'https://services.odata.org/V2/Northwind/Northwind.svc/$batch',
+        target: 'https://services.odata.org/V2/Northwind/Northwind.svc/',
         changeOrigin: true,
-        secure: false
-        // pathRewrite: {'^/api': ''}
+        secure: false,
+        pathRewrite: {'^/api': ''}
       })
   )
 };
